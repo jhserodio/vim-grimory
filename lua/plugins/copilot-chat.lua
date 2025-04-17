e269839fee29c3c524bbadccffff5fcf3d80e0ea
Joao Henrique Serodio <me@jhserodio.dev>
2025-04-17 14:55:53 -0300

    add new plugins: - vscode - highlight-undo

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -10,7 +10,7 @@ return {
       local user = vim.env.USER or "User"
       user = user:sub(1, 1):upper() .. user:sub(2)
       return {
-        model = "gpt-3.5-turbo",
+        model = "gpt-4o",
         auto_insert_mode = true,
         question_header = "  " .. user .. " ",
         answer_header = "  Copilot ",
